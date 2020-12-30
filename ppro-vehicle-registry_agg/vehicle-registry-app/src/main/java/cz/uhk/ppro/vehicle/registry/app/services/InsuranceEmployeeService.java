390bb55e268ada20537406405576688a1fe019c1
BuildTools <unconfigured@null.spigotmc.org>
2020-12-30 17:56:31 +0100

    oprava chybějící anotace

--- ppro-vehicle-registry_agg/vehicle-registry-app/src/main/java/cz/uhk/ppro/vehicle/registry/app/services/InsuranceEmployeeService.java
+++ ppro-vehicle-registry_agg/vehicle-registry-app/src/main/java/cz/uhk/ppro/vehicle/registry/app/services/InsuranceEmployeeService.java
@@ -4,7 +4,9 @@ import cz.uhk.ppro.vehicle.registry.common.VehicleRegistry;
 import cz.uhk.ppro.vehicle.registry.common.entities.InsuranceEmployee;
 import cz.uhk.ppro.vehicle.registry.common.exceptions.PersonException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class InsuranceEmployeeService {
     @Autowired
     private VehicleRegistry vehicleRegistry;

62ea23ffcf5e85141550da14b91a800e3c927686
BuildTools <unconfigured@null.spigotmc.org>
2021-01-04 22:08:45 +0100

    insurance employee místo user

--- ppro-vehicle-registry_agg/vehicle-registry-common/src/main/java/cz/uhk/ppro/vehicle/registry/common/entities/Insurance.java
+++ ppro-vehicle-registry_agg/vehicle-registry-common/src/main/java/cz/uhk/ppro/vehicle/registry/common/entities/Insurance.java
@@ -48,7 +48,7 @@ public class Insurance implements Serializable {
      */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "IDINSURANCER", nullable = false)
-    private InsuranceEmployee insurancer;
+    private User insurancer;
 
     /**
      * Člověk, který toto vozidlo pojistil
@@ -96,11 +96,11 @@ public class Insurance implements Serializable {
         this.vehicle = vehicle;
     }
 
-    public InsuranceEmployee getInsurancer() {
+    public User getInsurancer() {
         return insurancer;
     }
 
-    public void setInsurancer(InsuranceEmployee insurancer) {
+    public void setInsurancer(User insurancer) {
         this.insurancer = insurancer;
     }
 

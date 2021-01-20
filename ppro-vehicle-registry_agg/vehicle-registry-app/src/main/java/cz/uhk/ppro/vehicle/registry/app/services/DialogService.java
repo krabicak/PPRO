b4a168079b2560beb741942a68b0001660c558f1
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 19:32:00 +0100

    lep39 hl83en9 chzb

--- ppro-vehicle-registry_agg/vehicle-registry-app/src/main/java/cz/uhk/ppro/vehicle/registry/app/services/DialogService.java
+++ ppro-vehicle-registry_agg/vehicle-registry-app/src/main/java/cz/uhk/ppro/vehicle/registry/app/services/DialogService.java
@@ -5,14 +5,27 @@ import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.notification.Notification;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class DialogService {
 
     public void showErrorDialog(Exception e) {
         Dialog dialog = new Dialog();
-        if (e.getLocalizedMessage() == null)
+        if (e.getLocalizedMessage() == null) {
             dialog.add(new Text(e.toString()));
-        else
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            final String utf8 = StandardCharsets.UTF_8.name();
+            try (PrintStream ps = new PrintStream(baos, true, utf8)) {
+                e.printStackTrace(ps);
+                dialog.add(new Text(baos.toString(utf8)));
+            } catch (Exception ex) {
+                showErrorDialog(ex);
+            }
+        } else
             dialog.add(new Text(e.getLocalizedMessage()));
         dialog.open();
     }

e2dff69436011c52fc411114328251cf6035f1a9
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 14:49:09 +0100

    getloggeduser

--- ppro-vehicle-registry_agg/vehicle-registry-app/src/main/java/cz/uhk/ppro/vehicle/registry/app/services/SessionService.java
+++ ppro-vehicle-registry_agg/vehicle-registry-app/src/main/java/cz/uhk/ppro/vehicle/registry/app/services/SessionService.java
@@ -1,13 +1,18 @@
 package cz.uhk.ppro.vehicle.registry.app.services;
 
 import com.vaadin.flow.server.VaadinSession;
+import cz.uhk.ppro.vehicle.registry.common.VehicleRegistry;
 import cz.uhk.ppro.vehicle.registry.common.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SessionService {
 
     private static final String ATTRIBUTE_USERNAME = "login";
+
+    @Autowired
+    private VehicleRegistry vehicleRegistry;
 
     public boolean isLogged() {
         VaadinSession vaadinSession = VaadinSession.getCurrent();
@@ -19,6 +24,10 @@ public class SessionService {
         VaadinSession vaadinSession = VaadinSession.getCurrent();
         String username = (String) vaadinSession.getAttribute(ATTRIBUTE_USERNAME);
         return username;
+    }
+
+    public User getLoggedUser(){
+        return vehicleRegistry.getUserByLogin(getLogin());
     }
 
     public void saveLogin(String login){
